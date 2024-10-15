844f32c837da909065d2dea5d0944088677d09aa
kanuni <mister.nicto006@gmail.com>
2024-10-15 20:22:53 +0300

    fix

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,8 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import style from "./profile.module.scss";
-import Input from "../../components/Forms/Form/Input/Input";
 import Button from "../../components/Forms/Form/Button/Button";
 import img from "../../assets/Profile/7c15a2bafc0d0722be2b0d57a6be6c03.svg";
+import { auth, db } from "../../firebase"; // Импортируйте ваш файл с настройками Firebase
+
+import { SubmitHandler, useForm } from "react-hook-form";
+import addProfile from "../../server/addProfile/addProfile";
+import { onValue, ref } from "firebase/database";
 
 interface FormData {
   name: string;
@@ -13,33 +17,63 @@ interface FormData {
   drivingExperience: string;
   citizenship: string;
   inn: string;
+  userUID: string;
 }
 
 export const Profile = () => {
-  const [name, setName] = useState("");
-  const [surname, setSurname] = useState("");
-  const [birthDate, setBirthDate] = useState("");
-  const [phone, setPhone] = useState("");
-  const [email, setEmail] = useState("");
-  const [drivingExperience, setDrivingExperience] = useState("");
-  const [citizenship, setCitizenship] = useState("");
-  const [inn, setInn] = useState("");
+  const [profile, setProfile] = useState([]);
+
+  const { register, handleSubmit, setValue } = useForm<FormData>();
+
+  const submit: SubmitHandler<FormData> = async (data) => {
+    try {
+      await addProfile(data);
+
+      data.userUID = auth.currentUser.uid;
+    } catch (error) {
+      console.error("Ошибка при добавлении автомобиля:", error);
+    }
+  };
+
+  useEffect(() => {
+    const dataBaseProfile = ref(db, `/UsersProfile/${auth.currentUser.uid}`);
+
+    const unsubscribe = onValue(dataBaseProfile, (snapshot) => {
+      const profiles = snapshot.val() || [];
+      setProfile(profiles);
+
+      console.log(profile);
+
+      // Найдите профиль текущего пользователя
+      // const User = profiles.find((el) => el.userUID === auth.currentUser.uid);
+      // setCurrentUser(User || null);
+
+      // if (User) {
+      //   Object.keys(User).forEach((key) =>
+      //     setValue(key as keyof FormData, User[key])
+      //   );
+      // }
+    });
+
+    return () => unsubscribe(); // Очистите слушатель
+  }, [setValue]);
+
 
   return (
     <div className="container">
-      <div className={style.profile}>
+      <form onSubmit={handleSubmit(submit)} className={style.profile}>
         <h4>Ваш профиль</h4>
-        <h1> Базовые настройки</h1>
+        <h1>Базовые настройки</h1>
         <div className={style.info}>
           <div className={style.avatar}></div>
           <div className={style.settings}>
             <label htmlFor="file" className={style.settingsLabel}>
               <img src={img} alt="Добавить фотографию" />
               <input
-                id="file" // Добавляем id
+                className={style.fileInput}
+                id="file"
                 type="file"
                 placeholder="Добавить фотографию"
-                className={style.fileInput} // Добавим класс для стилизации, если нужно
               />
               <span>Добавить фотографию</span>
             </label>
@@ -48,80 +82,75 @@ export const Profile = () => {
         <div className={style.personal}>
           <h2>Личная Информация</h2>
           <label className={style.label}>
-            <Input
+            <input
               type="text"
               placeholder="Имя"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
+              {...register("name", { required: true })}
+              defaultValue={profile.name}
             />
           </label>
           <label className={style.label}>
-            <Input
+            <input
               className="input"
               type="text"
               placeholder="Фамилия"
-              value={surname}
-              onChange={(e) => setSurname(e.target.value)}
+              {...register("surname", { required: true })}
+              defaultValue={profile.surname}
             />
           </label>
           <label className={style.label}>
             <input
               type="date"
-              placeholder="День Рождения"
-              value={birthDate}
-              onChange={(e) => setBirthDate(e.target.value)}
+              defaultValue={profile.birthDate}
+              {...register("birthDate", { required: true })}
             />
           </label>
         </div>
         <div className={style.personal}>
           <h2>Дополнительная Информация</h2>
           <label className={style.label}>
-            <Input
+            <input
               type="number"
               placeholder="Телефон"
-              value={phone}
-              onChange={(e) => setPhone(e.target.value)}
+              {...register("phone", { required: true })}
+              defaultValue={profile.phone}
             />
           </label>
           <label className={style.label}>
-            <Input
-              className="input"
-              type="text"
+            <input
+              type="email"
               placeholder="E-mail"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              {...register("email", { required: true })}
+              defaultValue={profile.email}
             />
           </label>
           <label className={style.label}>
-            <Input
-              className="input"
-              type="text"
+            <input
+              type="number"
               placeholder="Водительский стаж"
-              value={drivingExperience}
-              onChange={(e) => setDrivingExperience(e.target.value)}
+              {...register("drivingExperience", { required: true })}
+              defaultValue={profile.drivingExperience}
             />
           </label>
           <label className={style.label}>
-            <Input
-              className="input"
+            <input
               type="text"
               placeholder="Гражданство"
-              value={citizenship}
-              onChange={(e) => setCitizenship(e.target.value)}
+              {...register("citizenship", { required: true })}
+              defaultValue={profile.citizenship}
             />
           </label>
           <label className={style.label}>
-            <Input
-              className="input"
-              type="text"
+            <input
+              type="number"
               placeholder="ИНН"
-              value={inn}
-              onChange={(e) => setInn(e.target.value)}
+              {...register("inn", { required: true })}
+              defaultValue={profile.inn}
             />
           </label>
         </div>
         <Button variant="">Редактировать</Button>
-      </div>
+      </form>
     </div>
   );
 };
