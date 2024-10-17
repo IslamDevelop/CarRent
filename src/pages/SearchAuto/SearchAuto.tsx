64419c7568a0e1e5ed24f55085159e2a9ce6ff2e
kanuni <mister.nicto006@gmail.com>
2024-10-17 22:07:05 +0300

    fix

--- src/pages/SearchAuto/SearchAuto.tsx
+++ src/pages/SearchAuto/SearchAuto.tsx
@@ -1,16 +1,23 @@
 import { onValue, ref } from "firebase/database";
 import { useEffect, useState } from "react";
-import { db } from "../../firebase";
 import SearchInput from "./SearchInput";
 import { useDispatch, useSelector } from "react-redux";
 import { addCar } from "../../store/slices/searchAvtoSlice";
 import { RootState } from "../../store/store";
+
+import { auth, db } from "../../firebase";
+
+import { rent } from "../../hooks/rent";
+import style from "./SearchAuto.module.css";
+import { useNavigate } from "react-router-dom";
+
 
 export const SearchAuto = () => {
   const cars = useSelector((state: RootState) => state.searchCar.cars);
   const [filteredCars, setFilteredCars] = useState(cars); // Состояние для отфильтрованных автомобилей
   const dispatch = useDispatch();
 
+  const [isauth, setAuth] = useState(false);
   const dataBaseCars = ref(db, `/Cars`);
   useEffect(() => {
     onValue(dataBaseCars, (snapshot) => {
@@ -23,7 +30,19 @@ export const SearchAuto = () => {
     setFilteredCars(cars);
   }, [cars]);
 
-  console.log(cars);
+  const navigate = useNavigate();
+  const redirectLogin = () => navigate("/Login");
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        setAuth(true);
+      } else {
+        setAuth(false);
+      }
+    });
+    return () => unsubscribe();
+  }, []);
 
   const handleSearch = (text: string) => {
     const lowercasedText = text.toLowerCase(); // Приводим текст к нижнему регистру для поиска
@@ -34,21 +53,47 @@ export const SearchAuto = () => {
     );
     setFilteredCars(filtered);
   };
+
   return (
-    <div>
-      <SearchInput onSearch={handleSearch} />
-      {filteredCars.length > 0 ? (
-        filteredCars.map((item, index) => (
-          <div key={index}>
-            <img src={item.carPhoto} alt={item.carName} />
-            <p>Марка: {item.carName } {item.carModel}</p>
-            <p>Год: {item.carYear}</p>
-            <p>Трансмиссия: {item.carTransmission}</p>
-          </div>
-        ))
-      ) : (
-        <p>Автомобили не найдены.</p>
-      )}
+    <div className={style.Find}>
+      <div className={style.FindContainer}>
+        <SearchInput onSearch={handleSearch} />
+
+        {filteredCars.map((item, index) => {
+          return (
+            <div key={item.carName + Math.random()} className={style.cardCar}>
+              <div className={style.cardLeft}>
+                <div className={style.cardHeader}>
+                  <p className={style.carModel}>{item.carModel}</p>
+                  <p className={style.carPrice}>от 150$ в сутки</p>
+                </div>
+                <img src={item.carPhoto} alt="Car" className={style.carImage} />
+              </div>
+              <div className={style.cardRight}>
+                <div className={style.cardDetails}>
+                  <p>Марка: {item.carName}</p>
+                  <p>Год: {item.carYear}</p>
+                  <p>Трансмиссия: {item.carTransmission}</p>
+                </div>
+                {isauth ? (
+                  item.isRented == false ? (
+                    <button
+                      className={style.BtnCardContain}
+                      onClick={(e) => rent(index, e)}
+                    >
+                      Арендовать
+                    </button>
+                  ) : (
+                    <button>Арендовано</button>
+                  )
+                ) : (
+                  <button onClick={() => redirectLogin()}>Арендовать</button>
+                )}
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
